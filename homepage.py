dc865fca146cf44667a413d93cbf1e120a93f5ff
NOORHAFIZAH BINTI MUHAMMAD <161951791+Kamsinah0606@users.noreply.github.com>
2025-10-29 22:02:31 +0800

    Create homepage.py

---
+++ homepage.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title="The indirect effect of TikTok use on depressive symptoms through insomnia among university students"
+)
+st.header("Data Visualization",divider="gray")
