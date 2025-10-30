946333792784ebd7500398ceec412cee0480e86c
NOORHAFIZAH BINTI MUHAMMAD <161951791+Kamsinah0606@users.noreply.github.com>
2025-10-30 11:55:33 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -21,7 +21,7 @@ objective_2 = st.Page("2_Social_Factors_vs_Addiction.py",
                       title="📈 Objective 2: Social Factors vs Addiction",
                       icon=":material/groups:")
 
-objective_3 = st.Page("Psychological_Behavioral_Trends.py",
+objective_3 = st.Page("3_Psychological_Behavioral_Trends.py",
                       title="🧠 Objective 3: Psychological & Behavioral Trends",
                       icon=":material/psychology:")
 
