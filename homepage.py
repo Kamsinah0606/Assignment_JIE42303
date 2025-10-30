9297a868c5f2b2947566192cdcafee3168c81e4e
NOORHAFIZAH BINTI MUHAMMAD <161951791+Kamsinah0606@users.noreply.github.com>
2025-10-30 10:09:57 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -4,19 +4,7 @@ st.set_page_config(
   page_title="The indirect effect of TikTok use on depressive symptoms through insomnia among university students"
 )
 st.header("Data Visualization",divider="gray")
-st.title("📊 Scientific Visualization – JIE42403")
-st.markdown("""
-Welcome to my scientific visualization dashboard!
 
-This project presents data analysis based on a real dataset from **DataBase.xlsx**, 
-exploring behavioral and demographic relationships through three objectives:
-1. **Demographic Distribution**
-2. **Social Factors vs Addiction**
-3. **Psychological and Behavioral Trends**
-
-Use the sidebar or page tabs to explore the visuals.  
-Developed by *NOORHAFIZAH BINTI MUHAMMAD*.
-""")
 
 visualise_1 = st.Page('1_Demographic_Distribution.py', title='Objective 1', icon=":material/school:")
 
@@ -30,3 +18,17 @@ pg = st.navigation(
 )
 
 pg.run()
+
+st.title("📊 Scientific Visualization – JIE42403")
+st.markdown("""
+Welcome to my scientific visualization dashboard!
+
+This project presents data analysis based on a real dataset from **DataBase.xlsx**, 
+exploring behavioral and demographic relationships through three objectives:
+1. **Demographic Distribution**
+2. **Social Factors vs Addiction**
+3. **Psychological and Behavioral Trends**
+
+Use the sidebar or page tabs to explore the visuals.  
+Developed by *NOORHAFIZAH BINTI MUHAMMAD*.
+""")
