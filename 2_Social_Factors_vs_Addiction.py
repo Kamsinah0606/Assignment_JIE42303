cf27a9859d7cd4b3dd29e1957a9b2310f71c9c47
NOORHAFIZAH BINTI MUHAMMAD <161951791+Kamsinah0606@users.noreply.github.com>
2025-10-30 10:28:47 +0800

    Update 2_Social_Factors_vs_Addiction.py

--- 2_Social_Factors_vs_Addiction.py
+++ 2_Social_Factors_vs_Addiction.py
@@ -6,11 +6,13 @@ import plotly.express as px
 
 st.title("2️⃣ Social Factors vs Addiction (BFAS Total)")
 
-DATA_URL = "https://raw.githubusercontent.com/<username>/Assignment_JIE42303/main/cleaned_dataset.csv"
+DATA_URL = "https://raw.githubusercontent.com/Kamsinah0606/Assignment_JIE42303/main/DataBase.csv"
 df = pd.read_csv(DATA_URL)
 
-st.subheader("Objective:")
+st.subheader("Objective 2:")
 st.info("To examine how social factors such as gender, age, and employment relate to addiction levels (BFAS Total).")
+
+st.markdown("---")
 
 # Visualization 4: Boxplot
 fig, ax = plt.subplots(figsize=(6,4))
@@ -18,9 +20,13 @@ sns.boxplot(x="Sex", y="BFAS total", data=df, ax=ax)
 ax.set_title("BFAS Total by Gender")
 st.pyplot(fig)
 
+st.markdown("---")
+
 # Visualization 5: Scatter plot
 fig = px.scatter(df, x="Age", y="BFAS total", color="Sex", title="Age vs BFAS Total")
 st.plotly_chart(fig, use_container_width=True)
+
+st.markdown("---")
 
 # Visualization 6: Heatmap
 pivot = df.pivot_table(index="Employment", columns="Economic status", values="BFAS total", aggfunc="mean")
