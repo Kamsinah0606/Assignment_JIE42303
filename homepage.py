5e3e2b7164c8c135c953d2c516f51d62298ccd3c
NOORHAFIZAH BINTI MUHAMMAD <161951791+Kamsinah0606@users.noreply.github.com>
2025-10-30 10:45:35 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -21,9 +21,9 @@ Developed by *NOORHAFIZAH BINTI MUHAMMAD*.
 # Navigation Buttons
 st.markdown("---")
 st.subheader(" Navigate to Objectives:")
-st.page_link('pages/1_Demographic_Distribution.py', label ='Objective 1: Demographic Distribution')
-st.page_link('pages/2_Social_Factors_vs_Addiction.py', label = 'Objective 2: Social Factors vs Addiction')
-st.page_link('pages/3_Psychological_Behavioral_Trends.py', label = 'Objective 3: Psychological and Behavioral Trends')
+st.page_link('1_Demographic_Distribution.py', label ='Objective 1: Demographic Distribution')
+st.page_link('2_Social_Factors_vs_Addiction.py', label = 'Objective 2: Social Factors vs Addiction')
+st.page_link('3_Psychological_Behavioral_Trends.py', label = 'Objective 3: Psychological and Behavioral Trends')
 
 home = st.Page('homepage.py', title='Homepage', default=True)
 
